3a7dfb7332877470d11e888b701a51125f4205dc
Giba-ljr <107215891+Giba-ljr@users.noreply.github.com>
2024-05-03 12:46:28 -0300

    Update server.js

--- src/services/server.js
+++ src/services/server.js
@@ -1,6 +1,6 @@
 function handleRequest(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, world!\n');
+    res.end('Hello, world!2\n');
   }
   
   module.exports = handleRequest;
