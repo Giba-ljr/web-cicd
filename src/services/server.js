42b5b0aec99474c37d9a0bb80162f056ab9d445b
Gilberto Lozano Junior <lozano.gilberto@outlook.com>
2024-05-03 15:33:17 -0300

    hello world

--- src/services/server.js
+++ src/services/server.js
@@ -1,7 +1,6 @@
 function handleRequest(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, world!2\n');
+    res.end('Hello, world!\n');
   }
   
   module.exports = handleRequest;
-  
